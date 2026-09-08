48fa777fa223dc6478bca67c3cdd7dc17f753618
Alexandre M.J.-C. Wadoux <alexandre.wadoux@yahoo.fr>
2026-09-08 17:37:41 +1000

    Declare ggplot aesthetics for package checks

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,6 @@
 utils::globalVariables(c(
-  "Model", "colvar", "lab", "labelc", "label_text", "label_x", "label_y",
-  "empirical", "model", "nME", "nominal", "r", "ticks", "uRMSDnorm_sigmaD", "x", "xcircle",
+  "Model", "colvar", "density", "lab", "labelc", "label_hjust",
+  "label_text", "label_vjust", "label_x", "label_y", "empirical", "model",
+  "nME", "nominal", "r", "ticks", "uRMSDnorm_sigmaD", "x", "xcircle",
   "xend", "y", "ycircle", "yend", "zero"
 ))
